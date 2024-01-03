abf3f9e829f484e80c6d97c74c6d2569cae43b6c
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2024-01-03 09:46:03 +0900

    img upload 기능 완성

---
+++ src/main/java/com/baro/domain/user/service/ImgUploadService.java
@@ -0,0 +1,37 @@
+package com.baro.domain.user.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ImgUploadService {
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadFile(MultipartFile file) {
+        try {
+            String fileName=file.getOriginalFilename();
+            String fileUrl= "https://" + bucket + "/test" +fileName;
+            ObjectMetadata metadata= new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            amazonS3Client.putObject(bucket,fileName,file.getInputStream(),metadata);
+            return fileUrl;
+        } catch (IOException e) {
+            log.info("upload file Error : {}" , e);
+            return  "fail";
+        }
+
+    }
+}

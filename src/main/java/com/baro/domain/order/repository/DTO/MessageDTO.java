e3eb59c4fb1bb69c55b2270232e5822ede2cb151
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2024-01-29 10:38:58 +0900

    rabbitmq setting -> test용

---
+++ src/main/java/com/baro/domain/order/repository/DTO/MessageDTO.java
@@ -0,0 +1,14 @@
+package com.baro.domain.order.repository.DTO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDTO {
+    private String title;
+    private String content;
+
+}

cc069abaa6321be3ce70b1777903178159f59226
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2024-02-17 00:24:41 +0900

    order data Read - 추가 .. orderCode

--- src/main/java/com/baro/domain/order/service/ReadOrderService.java
+++ src/main/java/com/baro/domain/order/service/ReadOrderService.java
@@ -18,11 +18,33 @@ import java.util.Optional;
 @Service
 public class ReadOrderService {
     private final JPAMongoOrderRepository orderRepository;
+
+    public OrderReadDAO read_order_orderCode_data_service(String orderCode){
+        OrderReadDAO orderReadData = null;
+        
+        try{
+            Optional<Order> option_orderData =  orderRepository.findByOrderCode(orderCode);
+            
+            if(option_orderData.isEmpty()){
+                //존재하지않음
+                return null;
+            }
+            Order orderData = option_orderData.get();
+            orderReadData = convertToOrderReadDAO(orderData);
+        }
+        catch (Exception e){
+            log.warn("error - readOrder_orderCode : {}",e);
+        }
+
+       return orderReadData;
+
+    }
+
     public OrderWaitingListDAO read_order_waitingList_service(String machine_id){
         OrderWaitingListDAO orderWaitingList = new OrderWaitingListDAO();
 
         List<Order> orderDataList = orderRepository.findByMachineIdAndStatus(machine_id , OrderStatus.WAITING);
-
+    
 
         List<OrderReadDAO> orderReadList = new ArrayList<>();
         for (Order order : orderDataList) {
@@ -44,4 +66,5 @@ public class ReadOrderService {
 
         return orderReadDAO;
     }
+    
 }
